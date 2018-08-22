140bb40885ec5c22a5ab6c622eccda279712ebe7
Sergey Kazakov <sergeykazakoff@yandex.ru>
2018-08-22 09:55:13 +0300

    :construction: In progress

--- index.mjs
+++ index.mjs
@@ -2,105 +2,120 @@
 import yargs from "yargs"
 import Redis from "ioredis"
 
-const { redisUrl } = yargs.usage("Usage: $0 --redis-url").option("redis-url", {
-  type: "string",
-  default: "redis://localhost:6379",
-}).argv
+const { redisUrl, getErrors } = yargs
+  .usage("Usage: $0")
+  .option("redisUrl", {
+    type: "string",
+    default: "redis://localhost:6379",
+  })
+  .option("getErrors", {
+    type: "boolean",
+    default: false,
+  }).argv
 
-const errorHandler = fn => (...args) => fn(...args).catch(console.log)
+const MASTER_KEY = "master"
 
-const PUBLISHER_KEY = "publisher"
+const MESSAGES_KEY = "messages"
 
-const SUBSCRIBERS_KEY = "subscribers"
-
-const MESSAGE_CHANNEL = "queue"
+const ERRORS_KEY = "errors"
 
 const PROCESS_ID = process.pid
+
+const master = async ({ redisClient, pid, isFirstCall = false }) => {
+  const currentMaster = await redisClient.get(MASTER_KEY)
+
+  if (currentMaster === null || Number(currentMaster) !== pid) {
+    return
+  }
+
+  if (isFirstCall) {
+    console.log(`Master ${pid} is sending messages`)
+  }
+
+  const message = Math.random()
+
+  await redisClient.rpush(MESSAGES_KEY, message)
+
+  console.log("send", message)
+
+  setTimeout(master, 500, { redisClient, pid })
+}
+
+const slave = async ({ redisClient, pid, isFirstCall = false }) => {
+  const currentMaster = await redisClient.get(MASTER_KEY)
+
+  if (currentMaster === null || Number(currentMaster) === pid) {
+    return
+  }
+
+  if (isFirstCall) {
+    console.log(`Slave ${pid} is getting messages`)
+  }
+
+  const message = await redisClient.rpop(MESSAGES_KEY)
+
+  if (message) {
+    console.log("get", message)
+
+    if (Math.random() < 0.05) {
+      await redisClient.rpush(ERRORS_KEY, message)
+
+      console.log(`Error ${message} was found`)
+    }
+  }
+
+  setTimeout(slave, 500, { redisClient, pid })
+}
 ;(async () => {
   const redis = new Redis(redisUrl)
 
-  let pub = null
+  if (getErrors) {
+    const [[, errors]] = await redis
+      .multi()
+      .lrange(ERRORS_KEY, 0, -1)
+      .del(ERRORS_KEY)
+      .exec()
 
-  let pubIntervalId = null
+    if (errors.length === 0) {
+      console.log("Errors are not found")
+    } else {
+      errors.forEach(err => console.log(err))
+    }
 
-  let sub = null
-
-  const publisherExists = await redis.exists(PUBLISHER_KEY)
-
-  if (publisherExists) {
-    sub = new Redis(redisUrl)
-
-    sub.subscribe(
-      [MESSAGE_CHANNEL, PROCESS_ID],
-      errorHandler(async () => {
-        await redis.rpush(SUBSCRIBERS_KEY, PROCESS_ID)
-
-        console.log(`Subscriber ${PROCESS_ID} is listening`)
-      }),
-    )
-
-    sub.on(
-      "message",
-      errorHandler(async (channel, message) => {
-        if (channel === MESSAGE_CHANNEL) {
-          return console.log(message)
-        }
-
-        await redis.lrem(SUBSCRIBERS_KEY, 0, PROCESS_ID)
-
-        sub.unsubscribe([MESSAGE_CHANNEL, PROCESS_ID])
-
-        pub = new Redis()
-
-        await redis.set(PUBLISHER_KEY, PROCESS_ID)
-
-        console.log(`Publisher ${PROCESS_ID} is starting`)
-
-        pubIntervalId = setInterval(
-          errorHandler(() => pub.publish(MESSAGE_CHANNEL, PROCESS_ID)),
-          500,
-        )
-      }),
-    )
-  } else {
-    pub = new Redis()
-
-    await redis.set(PUBLISHER_KEY, PROCESS_ID)
-
-    console.log(`Publisher ${PROCESS_ID} is starting`)
-
-    pubIntervalId = setInterval(
-      errorHandler(() => pub.publish(MESSAGE_CHANNEL, PROCESS_ID)),
-      500,
-    )
+    return process.exit(0)
   }
 
-  process.on(
-    "SIGINT",
-    errorHandler(async () => {
-      if (pub) {
-        await redis.del(PUBLISHER_KEY)
+  redis.on("ready", () => redis.config("SET", "notify-keyspace-events", "KEA"))
 
-        clearInterval(pubIntervalId)
+  const expirationListener = new Redis(redisUrl)
 
-        console.log(`Publisher ${PROCESS_ID} finished`)
+  expirationListener.subscribe("__keyevent@0__:expired")
 
-        const [firstSubscriber] = await redis.lrange(SUBSCRIBERS_KEY, 0, 1)
+  expirationListener.on("message", async () => {
+    try {
+      console.log("Master has been expired")
 
-        if (firstSubscriber) {
-          redis.publish(firstSubscriber, "start")
-        }
+      if ((await redis.set(MASTER_KEY, PROCESS_ID, "EX", 10, "NX")) === "OK") {
+        return master({
+          redisClient: redis,
+          pid: PROCESS_ID,
+          isFirstCall: true,
+        })
       }
 
-      if (sub) {
-        await redis.lrem(SUBSCRIBERS_KEY, 0, PROCESS_ID)
-      }
+      await slave({ redisClient: redis, pid: PROCESS_ID, isFirstCall: true })
+    } catch (err) {
+      console.log(err)
+    }
+  })
 
-      await Promise.all([
-        redis.quit(),
-        ...(pub ? [pub.quit()] : []),
-        ...(sub ? [sub.quit()] : []),
-      ])
-    }),
-  )
+  if ((await redis.set(MASTER_KEY, PROCESS_ID, "EX", 10, "NX")) === "OK") {
+    return master({
+      redisClient: redis,
+      pid: PROCESS_ID,
+      isFirstCall: true,
+    })
+  }
+
+  await slave({ redisClient: redis, pid: PROCESS_ID, isFirstCall: true })
 })().catch(console.log)
